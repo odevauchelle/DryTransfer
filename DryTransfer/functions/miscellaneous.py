0e3e2bde919557f2049121ba3879def5cb9e561b
Olivier Devauchelle <olivier@adour.ipgp.fr>
2021-01-11 17:45:05 +0100

    Added rounding function
    Correction of plot_frame third label position

--- DryTransfer/functions/miscellaneous.py
+++ DryTransfer/functions/miscellaneous.py
@@ -1,5 +1,5 @@
 
-def figname( file, ext = '.pdf' ):
+def figname( filename = None , ext = '.pdf' ):
     '''
     Generate figure name based on current script name.
 
@@ -16,9 +16,33 @@ def figname( file, ext = '.pdf' ):
         File name with a new extension
     '''
 
-    figure_name = file.split('/')[-1].split('.')[0] + ext
+    if filename is None :
+        filename = __file__
+
+    figure_name = filename.split('/')[-1].split('.')[0] + ext
 
     return figure_name
+
+def float2str( num, rounding_scale = 100 ) :
+    '''
+    Round number for display.
+
+    Arguments
+    ---------
+    num : float
+        The number to be displayed
+    rounding_scale : int
+        How many significant figures
+
+    Returns
+    -------
+    disp_num : str
+        String ready for display
+    '''
+
+    disp_num = round( num*rounding_scale )/float(rounding_scale)
+
+    return str( disp_num )
 
 ###################################
 #
--- DryTransfer/functions/plot_frame.py
+++ DryTransfer/functions/plot_frame.py
@@ -1,5 +1,5 @@
 from matplotlib.pyplot import gcf, gca
-from pylab import mean, array, norm
+from pylab import mean, array, norm, sqrt
 from matplotlib.pyplot import rcParams
 
 ###################################
@@ -40,7 +40,7 @@ def plot_frame( ax = None, center = ( 0., 0. ), arrow_length = 1., letters = ( '
         Name of axis
     orientation : 1 or -1
         Left or right
-    text_offset : float
+    z_offset : float
         Distance of third label from origin (in points)
     origin_marker : list
         Markers denoting origin
@@ -53,10 +53,12 @@ def plot_frame( ax = None, center = ( 0., 0. ), arrow_length = 1., letters = ( '
 
     if z_offset is None :
 
+        z_offset_to_fontsize_ratio = .6
+
         try :
-            z_offset = kwargs['fontsize']
+            z_offset = z_offset_to_fontsize_ratio*kwargs['fontsize']
         except :
-            z_offset = rcParams['font.size']
+            z_offset = z_offset_to_fontsize_ratio*rcParams['font.size']
 
     annotate_kwargs = _plot_frame_default_style.copy()
     annotate_kwargs['arrowprops']['color'] = color
@@ -86,22 +88,22 @@ def plot_frame( ax = None, center = ( 0., 0. ), arrow_length = 1., letters = ( '
     xytext = [ ( center[0], center[1] + orientation*arrow_length[1] ), ( center[0] + arrow_length[0], center[1]  ) ]
     xy = [ center ]*2
 
-    xytext += [ -mean( array( xytext ), axis = 0 ) ]
-    xy += [ center ]
-
     for i, letter in enumerate( letters ) :
 
-        direction = array( xytext[i] ) - array( xy[i] )
-        direction *= z_offset/norm( direction )
-
-        ghost_annotate_kwargs = annotate_kwargs.copy()
-        ghost_annotate_kwargs.update( arrowprops = None, textcoords = 'offset points' )
 
         if i < 2 :
             ax.annotate( '', xy = xy[i], xytext = xytext[i], **annotate_kwargs ) # arrow, two annotate calls for exact arrow length
+            direction = array( xytext[i] ) - array( xy[i] )
+            direction *= z_offset/norm( direction )
             ghost_xy = xytext[i]
         else :
+            direction = - mean( array( xytext ) - array( xy ), axis = 1 )
+            direction *= sqrt(2)*z_offset/norm( direction )
             ghost_xy = center
+
+
+        ghost_annotate_kwargs = annotate_kwargs.copy()
+        ghost_annotate_kwargs.update( arrowprops = None, textcoords = 'offset points' )
 
         ax.annotate( letter, xy = ghost_xy, xytext = direction, **ghost_annotate_kwargs ) # arrow label
 
