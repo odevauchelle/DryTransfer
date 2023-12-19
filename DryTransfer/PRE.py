76669936f202b420a2654d4cfc7fdc46aab54311
Olivier Devauchelle <olivier@adour.ipgp.fr>
2023-12-19 10:48:24 +0100

    PRE style

---
+++ DryTransfer/PRE.py
@@ -0,0 +1,54 @@
+
+from .default import *
+
+####################
+#
+#   Template name
+#
+####################
+
+name = 'PRL'
+full_name = 'Physical Review Letters'
+url = r'https://journals.aps.org/prl/authors'
+
+####################
+#
+#   Figure size
+#
+####################
+
+regular_figure_width = 8.6*1.35 # cm, https://journals.aps.org/prl/authors
+regular_figure_width *= 0.3937 # inches
+
+page_width = 2*regular_figure_width
+
+large_figure_width = 2*regular_figure_width
+
+regular_figure_size = array( [ 1, regular_aspect_ratio ] )*regular_figure_width
+large_figure_size = array( [ 1, regular_aspect_ratio ] )*large_figure_width
+
+####################
+#
+#   Matplotlib rcParms
+#
+####################
+
+mpl.rcParams['figure.figsize'] = regular_figure_size
+
+####################
+#
+#   Try it out
+#
+####################
+
+if __name__ == '__main__' :
+
+    from pylab import subplots, linspace, show, sin
+
+    fig, axs = subplots( nrows = 3, sharex = True )
+    x = linspace(-5,5,100)
+
+    for i, ax in enumerate(axs) :
+        ax.plot(x,sin(i*x))
+
+    show()
